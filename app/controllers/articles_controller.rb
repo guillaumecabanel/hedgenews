d2fe7a8c9a392e62d12ba12e7961cef55601ce0f
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2016-12-07 11:18:36 +0100

    fix home and atticle controller to play with aylien service

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,10 @@
 class ArticlesController < ApplicationController
 
   def index
-    session[:current_search] = params
-    @topic = params[:topic]
+    # session[:current_search] = params
+    @topic_search = params[:topic_search]
+    @stories = ::AylienAPI::GetStoriesService.new(topic_search: @topic_search).call
 
-    api_instance = AylienNewsApi::DefaultApi.new
-
-    opts = {
-      title: @topic,
-      # published_at_start: "NOW-31DAYS",
-      # published_at_end: "NOW",
-      language: ['fr'],
-      source_name: ["Libération", "Le Monde", "Le Figaro", "L'Express",
-                    "Le Parisien", "L'Humanité", "Nouvel Obs", "Le Point",
-                    "La Tribune", "Les Echos", "Marianne", "Le Canard Enchaîné",
-                    "La Croix", "20 Minutes", "Valeurs Actuelles"],
-      return: ["id", "title", "body", "summary", "source", "author", "keywords",
-                  "hashtags", "words_count", "categories", "sentiment", "language", "published_at", "links"],
-      sort_by: 'published_at',
-      per_page: 20
-    }
-
-    begin
-      #List stories
-      @result = api_instance.list_stories(opts)
-    rescue AylienNewsApi::ApiError => e
-      puts "Exception when calling DefaultApi->list_stories: #{e}"
-    end
 
   end
 
