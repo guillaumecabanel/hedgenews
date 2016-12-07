5137fbb56591eec547751d5e42b69f262e49d28d
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2016-12-07 14:07:11 +0100

    random_sort for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,19 @@ class Article < ApplicationRecord
 
   has_many :topic_articles
   has_many :topics, through: :topic_articles
+
+
+  def self.random_sort(articles = [], parameters = {})
+
+    random_articles = []
+
+    sources_id = []
+
+    articles.each do |article|
+      random_articles << article unless sources_id.include? article.source
+      sources_id << article.source unless sources_id.include? article.source
+    end
+
+    random_articles
+  end
 end
