3be1903ca53cf4f62de9fecd67f03c51bd07e502
Raphaelle Coudin <raphaelle.coudin@gmail.com>
2016-12-09 15:11:27 +0100

    opposite opinion button works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,15 +15,59 @@ class ArticlesController < ApplicationController
     end
 
     @opposite_media = {}
-    @opposite_media["Le Monde"] = "Valeurs Actuelles"
-    @opposite_media["Le Figaro"] = "Médiapart"
+    if @hash_source_url["Valeurs Actuelles"] != nil
+      @opposite_media["Le Monde"] = "Valeurs Actuelles"
+    elsif @hash_source_url["Le Figaro"] != nil
+      @opposite_media["Le Monde"] = "Le Figaro"
+    else
+      @opposite_media["Le Monde"] = "Le Point"
+    end
+
+    if @hash_source_url["Médiapart"] != nil
+      @opposite_media["Le Figaro"] = "Médiapart"
+    elsif @hash_source_url["Marianne"] != nil
+      @opposite_media["Le Figaro"] = "Marianne"
+    else
+      @opposite_media["Le Figaro"] = "Libération"
+    end
+
     @opposite_media["Libération"] = "Les Echos"
     @opposite_media["Les Echos"] = "Libération"
-    @opposite_media["L'Express"] = "Marianne"
-    @opposite_media["Le Point"] = "Médiapart"
+
+    if @hash_source_url["Marianne"] != nil
+      @opposite_media["L'Express"] = "Marianne"
+    elsif @hash_source_url["Médiapart"] != nil
+      @opposite_media["L'Express"] = "Médiapart"
+    else
+      @opposite_media["L'Express"] = "Libération"
+    end
+
+    if @hash_source_url["Médiapart"] != nil
+      @opposite_media["Le Point"] = "Médiapart"
+    elsif @hash_source_url["Marianne"] != nil
+      @opposite_media["Le Point"] = "Marianne"
+    else
+      @opposite_media["Le Point"] = "Libération"
+    end
+
     @opposite_media["Marianne"] = "La Tribune"
-    @opposite_media["La Croix"] = "Marianne"
-    @opposite_media["La Tribune"] = "Marianne"
+
+    if @hash_source_url["Marianne"] != nil
+      @opposite_media["La Croix"] = "Marianne"
+    elsif @hash_source_url["Médiapart"] != nil
+      @opposite_media["La Croix"] = "Médiapart"
+    else
+      @opposite_media["La Croix"] = "Libération"
+    end
+
+    if @hash_source_url["Marianne"] != nil
+      @opposite_media["La Tribune"] = "Marianne"
+    elsif @hash_source_url["Médiapart"] != nil
+      @opposite_media["La Tribune"] = "Médiapart"
+    else
+      @opposite_media["La Tribune"] = "Libération"
+    end
+
     @opposite_media["Médiapart"] = "Le Point"
     @opposite_media["Valeurs Actuelles"] = "Le Monde"
 
