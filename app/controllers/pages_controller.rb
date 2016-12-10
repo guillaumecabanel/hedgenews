06d63a77ca35a716d4aa43df2094683fe11cc61b
Julie Moiton <julie.moiton@bluewin.ch>
2016-12-10 15:54:35 +0100

    update navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
     @topics = Topic.all.sample(3)
+    # @topic_names = []
+    # Topic.all.each do |topic|
+    #   @topic_names << topic.name
+    # end
+
   end
 
   def styleguide
